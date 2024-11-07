f9f80e86fd3b9bf9f506c5c2f31d8876706f8e6b
Angel <angel@mail.kway.com.tw>
2024-11-07 10:56:06 +0800

    <新增>程式碼

---
+++ ScheduleDemo_1735/Printer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace ScheduleDemo_1735
+{
+    public class Printer
+    {
+        public class PrintPerSeconds : IJob
+        {
+            public Task Execute(IJobExecutionContext context)
+            {
+                Console.WriteLine($"{DateTime.Now}: 記錄打印。");
+                return Task.CompletedTask;
+            }
+        }
+        public class PrintPerTime : IJob
+        {
+            public Task Execute(IJobExecutionContext context)
+            {
+                Console.WriteLine($"{DateTime.Now}：紀錄指定時間打印。");
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
--- ScheduleDemo_1735/Program.cs
+++ ScheduleDemo_1735/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Quartz.Impl;
 using Quartz;
+using System.Globalization;
 
 namespace ScheduleDemo_1735
 {
@@ -7,43 +8,106 @@ namespace ScheduleDemo_1735
     {
         static async Task Main(string[] args) // 非同步
         {
-            // 用戶輸入間隔時間 N 秒
-            int intervalInSeconds;
-            while (true)
+            // 輸入判定
+            #region
+            // 每 N秒 打印⼀次
+            Console.Write("每 N 秒打印一次，請輸入一個正整數：");
+            string inputSeconds = Console.ReadLine(); // 將使用者輸入的內容給inputSeconds
+            int valueSeconds;
+
+            // 驗證數字，使用 int.TryParse轉換inputSeconds檢查是否為數字
+            while (!int.TryParse(inputSeconds, out valueSeconds) || valueSeconds <= 0)
             {
-                Console.WriteLine("請輸入每隔 N 秒執行的間隔時間（秒）：");
+                Console.Write("無效輸入，請重新輸入一個正整數：");
+                inputSeconds = Console.ReadLine();
 
-                // 使用 TryParse 來檢查是否為有效的數字
-                string input = Console.ReadLine();
-                if (int.TryParse(input, out intervalInSeconds) && intervalInSeconds > 0)
-                {
-                    break;  // 解析成功並且數字大於0，則跳出循環
-                }
-                else
-                {
-                    Console.WriteLine("無效輸入，請輸入一個正整數。");
-                }
             }
+            Console.WriteLine($"【將每{valueSeconds}秒打印一次】");
 
-            // 用戶輸入指定時間
-            DateTime specifiedTime;
-            while (true)
+            // 設定指定時間打印⼀次
+            Console.Write("指定時間打印一次，請輸入指定時間(格式HHmmss)：");
+            string inputTime = Console.ReadLine();
+            DateTime valueTime;
+
+            // 驗證時間格式
+            // DateTime.TryParseExact(被解析字串inputTime, 指定格式, 區域設置[註1], 日期時間樣式[註2], 輸出解析後字串valueTime)
+            while (!DateTime.TryParseExact(inputTime, "HHmmss", null, DateTimeStyles.None, out valueTime))
             {
-                Console.WriteLine("請輸入指定的時間（格式：yyyy-MM-dd HH:mm:ss）：");
-
-                // 使用 TryParse 來檢查是否為有效的數字
-                string timeInput = Console.ReadLine();
-                if (DateTime.TryParse(timeInput, out specifiedTime))
-                {
-                    break;  // 解析成功並且數字大於0，則跳出循環
-                }
-                else
-                {
-                    Console.WriteLine("無效輸入，請重新輸入時間格式（yyyy-MM-dd HH:mm:ss）！");
-                }
+                Console.Write("無效輸入，請重新輸入指定時間(格式HHmmss)：");
+                inputTime = Console.ReadLine();
             }
+            Console.WriteLine($"【將在{valueTime}進行打印】");
+            #endregion
 
-            //= DateTime.Parse(Console.ReadLine());
+
+            // 創建 Scheduler
+            #region
+            var schedulerFactory = new StdSchedulerFactory();
+            var scheduler = await schedulerFactory.GetScheduler();  // 取得Scheduler
+            await scheduler.Start(); // 啟用
+
+            // 設定第一個排程（每 N 秒打印一次）
+            // 定義Job，使用 JobBuilder 建立作業
+            IJobDetail job1 = JobBuilder.Create<PrintPerSeconds>()
+                .WithIdentity("job1", "group1")  // 指定識別符，由名稱（job1）和組別（group1）組成
+                .Build();
+
+            // 定義Trigger，使用TriggerBuilder建立作業
+            ITrigger trigger1 = TriggerBuilder.Create()
+                .WithIdentity("trigger1", "group1") // 指定識別符
+                .StartNow()
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInSeconds(valueSeconds)
+                    .RepeatForever())
+                .Build();
+            // WithSimpleSchedule() 方法是用來定義觸發器的簡單執行規則。
+            // 配置作業執行的間隔時間(WithIntervalInSeconds)以及重複次數(RepeatForever)。
+
+            // 註冊作業和觸發器
+            await scheduler.ScheduleJob(job1, trigger1);
+
+            // 設定第二個排程（在指定時間打印一次）
+            IJobDetail job2 = JobBuilder.Create<PrintPerTime>()
+                .WithIdentity("job2", "group2")
+                .Build();
+
+            ITrigger trigger2 = TriggerBuilder.Create()
+                .WithIdentity("trigger2", "group2")
+                .StartAt(valueTime)
+                .Build();
+
+            await scheduler.ScheduleJob(job2, trigger2);
+
+            Console.WriteLine("排程已設定。按任意鍵退出...");
+            Console.ReadKey();
+            #endregion
         }
     }
+
+    // 打印訊息的 Job 類別
+    public class PrintPerSeconds : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine($"{DateTime.Now}: 記錄打印。");
+            return Task.CompletedTask;
+        }
+    }
+    public class PrintPerTime : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine($"{DateTime.Now}：紀錄指定時間打印。");
+            return Task.CompletedTask;
+        }
+    }
+
 }
+
+
+// 註1(區域設置)：IFormatProvider，null：默認使用當前文化
+// CultureInfo：指定文化，創建 CultureInfo 物件，並將其作為參數傳入。
+//              舉例 var germanCulture = new System.Globalization.CultureInfo("de-DE");
+
+// 註2(日期時間樣式)：
+// DateTimeStyles.None：不使用任何額外樣式，通常用這個。
